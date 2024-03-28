19ec25b3c64c65ead71c816a025c87ff8eee26b2
Oksana Bebesh <oksana.bebesh1984@gmail.com>
2024-03-28 15:31:04 +0100

    feat: one test passed

--- src/tests/CurrencyFactory/CurrencyFactoryTest.php
+++ src/tests/CurrencyFactory/CurrencyFactoryTest.php
@@ -3,24 +3,30 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject;
 use App\Factories\ApiCurrencyFactory;
 use App\Services\FreecurrencyApiService;
 use App\Services\NBPplApiService;
+use App\Interfaces\CurrencyConverterInterface;
 
 class CurrencyFactoryTest extends TestCase
 {
-    /**
-     * @dataProvider getCurrencyApi
-     */
-    public function testCreateCurrencyApi(string $params)
-    {
-        $argumentToCheck = ApiCurrencyFactory::createCurrencyApi($params);
-        echo ($argumentToCheck);
-        $this->assertSame($params, $argumentToCheck);
+    private MockObject $objectService;
+
+    protected function setUp():void {
+
+        // $this->objectService = $this->createMock(CurrencyConverterInterface::class);
+        $this->factory = new ApiCurrencyFactory();
     }
 
-    public function getCurrencyApi() {
-        yield ['NBPplApiService', new NBPplApiService()];
-        yield ['FreecurrencyApiService', new FreecurrencyApiService()];
+    public function testCreateCurrencyApi()
+    {
+        $param = 'Unknown Service Name';
+        $waitingForResult = 'Invalid CurrencyApi type specified.';
+
+        $result = $this->factory->createCurrencyApi($param);
+
+        $this->assertSame($waitingForResult, $result);
     }
+
 }
